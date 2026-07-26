fcc1ff326d7205a2ddc133d4636c5c525c09001e
Wittaya-Tasu <vetza.yayo@gmail.com>
2026-07-26 20:33:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // OAuth Client ID และ Spreadsheet ID ไม่ใช่รหัสลับ แต่ควรจำกัดสิทธิ์ใช้งานให้ถูกต้อง
 
 const APP_CONFIG = {
-    GOOGLE_CLIENT_ID: "YOUR_GOOGLE_CLIENT_ID_HERE",
-    SPREADSHEET_ID: "YOUR_SPREADSHEET_ID_HERE"
+    GOOGLE_CLIENT_ID: "971390460399-u8r8ct3fipkpg1vdjihgbtsbe8h2jgii.apps.googleusercontent.com",
+    SPREADSHEET_ID: "1xNasmNnUzlQQI3saK9mwiBjvWiJJOFj7-qznnfeAC2g"
 };
