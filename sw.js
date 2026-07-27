07a6d90d37b7df36cab61ee343aaf881290a6eb4
Wittaya-Tasu <vetza.yayo@gmail.com>
2026-07-27 20:31:32 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "personal-wealth-shell-v2.0.0";
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./config.js",
+  "./analytics.js",
+  "./api.js",
+  "./app.js",
+  "./manifest.json",
+  "./icons/wealth-icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+  const isSameOrigin = requestUrl.origin === self.location.origin;
+
+  // Never cache Google API or financial-data responses.
+  if (!isSameOrigin || event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && STATIC_ASSETS.some((asset) => requestUrl.pathname.endsWith(asset.replace("./", "/")))) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
